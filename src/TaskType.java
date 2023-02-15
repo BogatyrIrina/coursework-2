0c817ce4bf6ec8725e93a1572965b4d0716ce44f
admin <kochereva_irina@mail.ru>
2023-02-16 01:29:12 +0300

    Часть 1 Курсового проекта выполнена

---
+++ src/TaskType.java
@@ -0,0 +1,21 @@
+public enum TaskType {
+    WORK(" рабочие задачи. "),
+    PERSONAL(" личные задачи. ");
+    private final String taskType;
+
+
+    TaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskType{" +
+                "taskType='" + taskType + '\'' +
+                '}';
+    }
+}
